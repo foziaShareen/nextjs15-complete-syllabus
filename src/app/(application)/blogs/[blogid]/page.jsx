4433b3ede6f9e1ee3c7a04b68553ea627f933646
foziaShareen <foziashareen1@gmail.com>
2025-05-14 20:24:40 +0500

    static site generation

--- src/app/(application)/blogs/[blogid]/page.jsx
+++ src/app/(application)/blogs/[blogid]/page.jsx
@@ -7,6 +7,29 @@ export async function generateMetadata({ params }) {
     title : `Blog: ${blogid}`
 }
 }
+//if we want to stop more dynamic pages use this and set dynamicParams to false
+// export const dynamicParams = false
+
+export  async function generateStaticParams() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await res.json()
+   return  data.map(({id})=>({ blogid:`${id}`})
+)
+
+    
+    // return [
+    //     { blogid: '1' },
+    //     { blogid: '2' },
+    //     { blogid: '3' },
+    //     { blogid: '4' },
+    //     { blogid: '5' },
+    //     { blogid: '6' },
+    //     { blogid: '7' },
+    //     { blogid: '8' },
+    //     { blogid: '9' },
+    //     { blogid: '10' }
+    // ]
+}
 
 const blogid =async ({params}) => {
     const { blogid } =await params;
